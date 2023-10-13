25e99dc164c1f26354912054669ad202ee341c8d
NicoWATTEN <125352739+NicoWATTEN@users.noreply.github.com>
2023-10-13 17:13:13 +0200

    Add files via upload

--- python-app-fin/main.py
+++ python-app-fin/main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
+
 
 def hello_world():
    prefix_google = """
@@ -18,8 +19,17 @@ def hello_world():
    """
    return prefix_google + "Hello World"
 
+@app.route('/logger')
+def logger():
+    # Print a log in the Python console
+    print("This is a Python log message")
 
-
-@app.route("/")
-def root():
-   return "Hello from Space!"
+    # Print a log in the browser console using JavaScript
+    return """
+    <script>
+         console.log("This is a browser log message");
+     </script>
+     """
+   
+if __name__ == "__main__":
+     app.run()
