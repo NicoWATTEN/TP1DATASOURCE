99cabd9322c6262e0afbdd7003bad2618614328c
NicoWATTEN <125352739+NicoWATTEN@users.noreply.github.com>
2023-10-15 17:29:45 +0200

    tp2.end

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ import requests
 
  
 
-from google.oauth2.credentials import Credentials
-
-from google_auth_oauthlib.flow import InstalledAppFlow
-
-from googleapiclient.discovery import build
-
  
 app = Flask(__name__)
 
